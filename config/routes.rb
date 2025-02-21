a46425fe53373ec565d3cf6f676add0da43b8a37
ChangSonia <79300461+ChangSonia@users.noreply.github.com>
2025-02-21 21:51:00 +0000

    delete movie

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,6 @@ Rails.application.routes.draw do
   
   get("/actors", { :controller => "actors", :action => "index" })
   get("/actors/:path_id", { :controller => "actors", :action => "show" })
+
+  get("/delete_movie/:an_id", {:controller => "movies", :action => "destroy"})
 end
